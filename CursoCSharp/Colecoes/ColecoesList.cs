ff26a059f6358ef3468c5bd2b507de7add54f5a8
Marcos.Rael <marcos.rael@sequor.com.br>
2021-08-03 08:07:09 -0300

    chore: Coleções List<>

--- CursoCSharp/Colecoes/ColecoesList.cs
+++ CursoCSharp/Colecoes/ColecoesList.cs
@@ -23,6 +23,36 @@ namespace CursoCSharp.Colecoes {
 
             var carrinho = new List<Produto>();
 
+            carrinho.Add(livro);
+
+            var combo = new List<Produto>
+            {
+                new Produto("camisa", 29.9),
+                new Produto("8ª Temporada Game of Thrones", 99.9),
+                new Produto("Poster",10)
+            };
+
+            //Adiciona varios itens
+            carrinho.AddRange(combo);
+
+            Console.WriteLine(carrinho.Count);
+            carrinho.RemoveAt(3);
+
+            foreach(var item in carrinho)
+            {
+                Console.Write(carrinho.IndexOf(item));
+                Console.WriteLine($" {item.Nome} {item.Preco}");
+            }
+
+            carrinho.Add(livro);
+            carrinho.Add(livro);
+
+            //Retorna o index do ultimo registro adicionado com esse objeto
+            Console.WriteLine(carrinho.LastIndexOf(livro));
+
+            //Verifica se contém o objeto na lista
+            var result = carrinho.Contains(livro);
+            
         }
     }
 }
