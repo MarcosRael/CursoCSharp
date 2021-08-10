46e631d3eb042145bddefdff3b4458a37d9a2897
Marcos.Rael <marcos.rael@sequor.com.br>
2021-08-10 08:05:42 -0300

    chore: Coleções HashSet<>

---
+++ CursoCSharp/Colecoes/ColecoesSet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Colecoes {
+     
+    class ColecoesSet {
+
+      
+        public static void Executar() {
+
+            var livro = new Produto("Game of Throne", 49.9);
+
+            var carrinho = new HashSet<Produto>(); // HashSet é uma lista não indexada que não aceita repetição.
+
+            carrinho.Add(livro);
+
+            var combo = new HashSet<Produto>
+            {
+                new Produto("camisa", 29.9),
+                new Produto("8ª Temporada Game of Thrones", 99.9),
+                new Produto("Poster",10)
+            };
+
+            //Adiciona varios itens
+            carrinho.UnionWith(combo);//Realiza a união
+
+            Console.WriteLine(carrinho.Count);
+            //carrinho.RemoveAt(3);
+
+            foreach(var item in carrinho)
+            {
+                //Console.Write(carrinho.IndexOf(item));
+                Console.WriteLine($" {item.Nome} {item.Preco}");
+            }
+
+            
+            Console.WriteLine(carrinho.Count);
+            carrinho.Add(livro);
+            Console.WriteLine(carrinho.Count);//Não aceita inserir duas vezes a mesma estrutura.
+
+            //Retorna o index do ultimo registro adicionado com esse objeto
+            //Console.WriteLine(carrinho.LastIndexOf(livro));
+
+            //Verifica se contém o objeto na lista
+            var result = carrinho.Contains(livro);
+            
+        }
+    }
+}
