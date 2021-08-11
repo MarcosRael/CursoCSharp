228152b38ad7d148324a009d4b4f15efb38c0899
Marcos.Rael <marcos.rael@sequor.com.br>
2021-08-11 08:32:06 -0300

    chore: Coleções Stack

---
+++ CursoCSharp/Colecoes/ColecoesStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace CursoCSharp.Colecoes
+{
+    class ColecoesStack
+    {
+        public static void Executar()
+        {
+            //Pilha primeiro a entrar é o ultimo a sair
+            var pilha = new Stack();
+
+            pilha.Push(3);
+            pilha.Push("a");
+            pilha.Push(true);
+            pilha.Push(3.14f);
+
+            foreach (var item in pilha)
+            {
+                Console.WriteLine($"{item}, ");
+            }
+
+            //Retorna e remove o primeiro item da Pilha 
+            Console.WriteLine($"\nPop: {pilha.Pop()}");
+
+            foreach(var item in pilha)
+            {
+                Console.WriteLine($"{item}, ");
+            }
+
+            //Apenas retorna o primeiro item da Pilha
+            Console.WriteLine($"\nPeek: {pilha.Peek()}");
+            Console.WriteLine(pilha.Count);
+
+        }
+    }
+}
