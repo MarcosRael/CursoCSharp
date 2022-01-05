f4f7bc422aacd34e4e4912420362aed0dbf1f54e
MarcosRael <marcosasrael@gmail.com>
2022-01-05 08:31:18 -0300

    chore: Explorando API - Usando DirectoryInfo

---
+++ CursoCSharp/Api/ExemploDirectoryInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace CursoCSharp.Api
+{
+    class ExemploDirectoryInfo
+    {
+        public static void Executar()
+        {
+
+            var dirProjeto = @"C:/Projetos/Treinamentos/Cursos/CursoCSharp/CursoCSharp";
+
+            var dirInfo = new DirectoryInfo(dirProjeto);
+
+            if(!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            Console.WriteLine("== Arquivos ================");
+            var arquivos = dirInfo.GetFiles();
+            foreach(var arquivo in arquivos)
+            {
+                Console.WriteLine(arquivos);
+            }
+
+            Console.WriteLine("== Diretorios ==============");
+            var pastas = dirInfo.GetDirectories();
+            foreach(var pasta in pastas)
+            {
+                Console.WriteLine(pasta);
+            }
+
+            Console.WriteLine(dirInfo.CreationTime);
+            Console.WriteLine(dirInfo.FullName);
+            Console.WriteLine(dirInfo.Root);
+
+        }
+    }
+}
